ccc13b5666767ece2c7f65f407dd4c5897e4274b
barbaramootian <barbaramootian@student.moringaschool.com>
2021-11-29 13:24:13 +0300

    added the form functionality

--- js/main.js
+++ js/main.js
@@ -8,15 +8,42 @@ function Makeorder(flavor, crust, toppy, size, total) {
 }
 $(document).ready(function () {
     $("#home").click(function () {
-        prompt("Enter your location")
-        alert("Your order will be delivered to your location.");
+        var location = prompt("Enter your location")
+        if (!location) {
+            alert("Please enter your location")
+        } else {
+            alert("Your order will be delivered to your location.");
+        }
     });
 });
 $(document).ready(function () {
-    $("#make-order").click(function (e) {
-        var flavor = $("#flavor Option:selected").val();
-        var crust = $("#crust Option:selected").val();
-        var size = $("#size Option:selected").val();
-        var toppy = $("#toppy Option:selected").val();
+    $("#check").click(function () {
+        alert("Thanks for  making order")
+    });
+});
+$(document).ready(function () {
+    $("#form").submit(function (e) {
+        e.preventDefault()
+        var flavour = $("#flavor").val();
+        $("#flavour").text(flavour)
+        var crusty = $("#crust").val();
+        $("#crusty").text(crusty)
+        var topping = $("#toppy").val();
+        $("#topping").text(topping)
+        var sizing = $("#size").val();
+        $("#sizing").text(sizing)
+        if (sizing == "small") {
+            var price = 500;
+        } else if (sizing == "medium"){
+            var prize = 1000;
+        }else {
+            var prizing = 1500;
+        }
+        var crustPrize = 300 
+
+        var toppyPrice = 80 
+
+
     });
 })
+
